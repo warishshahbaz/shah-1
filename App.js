dda59621e37bb030eb2b7010701c02b157ae7295
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2022-02-10 10:07:40 +0530

    react app

---
+++ App.js
@@ -0,0 +1,57 @@
+
+// import './App.css';
+import React from 'react';
+import Practice from "./component/practice";
+
+
+// import Covid from './component/Covid';
+
+
+function App() {
+ 
+  return(
+    <div className=' ui container'>
+    <Practice
+      imgsrc="https://m.media-amazon.com/images/I/71Cx-rnPQAL._SL1368_.jpg"
+      title="Netflix Series"
+      sname="Dark"
+      link="https://www.google.com/search?q=movies+poster+images&client=firefox-b-d&biw=726&bih=730&sxsrf=APq-WBs1RSSVhaTNS_GFEfbvkgR36Y8-6w:1644394776139&tbm=isch&source=iu&ictx=1&vet=1&fir=8q9e_nH0xCAYhM%252C6o9KLSnIsVM5OM%252C_%253BVcmD2Tb1SK6pSM%252CUith2Al6mASjjM%252C_%253BmHQQTD9JphYQZM%252CuI8kpHwuGUv4TM%252C_%253Bqjvq7DCnSXM4AM%252CSQ61q3HC6-pnjM%252C_%253BhF9xEdlCRwhqBM%252CXkLx1CpzlHQQkM%252C_%253BzvqoBJMDu-JPHM%252C6o9KLSnIsVM5OM%252C_%253Be_j8o6HDBoshEM%252CPybT_wqEQ2qkNM%252C_%253BdABi65ZMSYHIJM%252Cvjp5H0PbNCix1M%252C_%253BdzWACmfsDfGcIM%252CR9uaJ4Or6UW2EM%252C_%253BihnlbFcCyl_zKM%252CjwJfzwCWAGfRPM%252C_%253Bqkl4lsEM0TXD5M%252CjaEx9FGhn1FzpM%252C_%253BXfkvCkYLjIa5fM%252CPybT_wqEQ2qkNM%252C_%253B_cPgSk6Nbm08aM%252CUith2Al6mASjjM%252C_%253BOT6opPuCrmvFcM%252CZS1ynP1Gnf12CM%252C_%253B-o3gS1_TrwvrHM%252CqL7zCpOn438qbM%252C_%253BNBY78Znbo7M0jM%252CNsrHOYZ1MMpGTM%252C_%253B_nYMTTTYJ7rgWM%252CPybT_wqEQ2qkNM%252C_%253BvuWZdTkWcLRvMM%252C_G-bXpCVL3vZ3M%252C_%253BkuPFWNnbl6MYwM%252CKD_TfCWmwtUdMM%252C_%253BnGq_lZaHHMyotM%252CSQ61q3HC6-pnjM%252C_%253BsvTNy31HnSSeXM%252CVI_0MM-oBmlofM%252C_&usg=AI4_-kSm1m503d5AlR2xT60xu-VNx39n_g&sa=X&ved=2ahUKEwjH1YH0l_L1AhX-SmwGHQU9BiEQ9QF6BAgDEAE#imgrc=mHQQTD9JphYQZM" 
+
+    />
+    <Practice
+      imgsrc="https://media.gettyimages.com/photos/pirates-of-the-caribbean-on-stranger-tides-poster-picture-id458587791?s=612x612"
+      title="Netflix Series"
+      sname="Pirates of Ocean"
+      link=""
+    />
+    <Practice
+      imgsrc="https://c8.alamy.com/comp/F762XE/film-movie-poster-of-titanic-F762XE.jpg"
+      title="Netflix Series"
+      sname="Titanic"
+      link=""
+    />
+    <Practice
+       imgsrc="https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg"
+      title="Netflix Series"
+      sname="Titanic"
+      link=""
+    />
+    <Practice
+      imgsrc="https://rukminim1.flixcart.com/image/416/416/jfea93k0/poster/j/3/u/medium-the-avenges-initiative-3-moives-posters-mp0098-original-imaf3mhpcyhwhtkt.jpeg?q=70"
+      title="Netflix Series"
+      sname="Titanic"
+      link=""
+    />
+    {/* <Covid/> */}
+      
+      
+     </div>
+  ); 
+
+ 
+}
+
+
+
+
+export default App;
